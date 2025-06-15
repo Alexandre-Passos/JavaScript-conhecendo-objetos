40b8ceb088744c9fc8b5342dfa8bb6bdca327e76
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-15 14:30:21 -0300

    objetos de objetos

---
+++ Aula-38-obj-obj.js
@@ -0,0 +1,17 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['551199999998', '551199999993'],
+}
+
+estudante.endereco = {
+    rua: 'Avenida Amazonas',
+    numero: 115,
+    complemento: 'Lanchonete do Elzir'
+}
+
+console.log(estudante)
+console.log(estudante.endereco.complemento)
